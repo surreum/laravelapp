b6dad2d9b926acdd6c182a4789844bcda1fcacf0
Surreum <surreum@gmail.com>
2021-07-16 23:03:50 +0300

    My fist blog posts

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My first Laravel App</title>
+    <link rel="stylesheet" href="/app.css" >
+</head>
+
+<body>
+    
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+            officia deserunt mollit anim id est laborum."</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+            officia deserunt mollit anim id est laborum."</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+        <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor 
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+            officia deserunt mollit anim id est laborum."</p>
+    </article>
+
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('/posts/{post}', function($slug){
+
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+        if(! file_exists($path)) {
+            return redirect("/");
+        }
+
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})-> where ('post', '[A-z_\-]+');
